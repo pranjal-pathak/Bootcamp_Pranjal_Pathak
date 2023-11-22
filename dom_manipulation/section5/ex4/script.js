98d3ebf3b21133ea0d5bd030f3b9fe42400ddfbc
pranjal-pathak <pranjalpathak510@gmail.com>
2023-11-22 14:52:47 +0530

    updated assignments

--- dom_manipulation/section5/ex4/script.js
+++ dom_manipulation/section5/ex4/script.js
@@ -10,21 +10,5 @@ document.addEventListener("DOMContentLoaded", function () {
   window.addEventListener("scroll", function () {
     const progress = (window.scrollY / totalHeight) * 100;
     progressBar.style.width = `${progress}%`;
-
-    // Show/hide the progress bar based on scroll position
-    if (progress > 0) {
-      progressContainer.style.display = "block";
-    } else {
-      progressContainer.style.display = "none";
-    }
-  });
-
-  // Update the progress bar on window resize
-  window.addEventListener("resize", function () {
-    // Recalculate the total scrollable height on resize
-    const newTotalHeight = document.body.scrollHeight - window.innerHeight;
-    const newProgress = (window.scrollY / newTotalHeight) * 100;
-    progressBar.style.width = `${newProgress}%`;
-    totalHeight = newTotalHeight; // Update the total height
   });
 });
