520840ea9a3026faa4fd2fad8b41dc06f43d2bce
pranjal-pathak <pranjalpathak510@gmail.com>
2023-11-28 05:13:27 +0530

    updated assignments

--- dom_manipulation/section4/ex1/script.js
+++ dom_manipulation/section4/ex1/script.js
@@ -1,18 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   const filterInput = document.getElementById("filterInput");
   const itemList = document.getElementById("itemList");
-  const items = itemList.getElementsByTagName("li");
+  const items = Array.from(itemList.getElementsByTagName("li"));
 
   filterInput.addEventListener("input", () => {
     const filterValue = filterInput.value.toLowerCase();
 
-    for (let i = 0; i < items.length; i++) {
-      const textContent = items[i].textContent.toLowerCase();
-      if (textContent.includes(filterValue)) {
-        items[i].style.display = "";
-      } else {
-        items[i].style.display = "none";
-      }
-    }
+    items.forEach((item) => {
+      const textContent = item.textContent.toLowerCase();
+      item.style.display = textContent.includes(filterValue) ? "" : "none";
+    });
   });
 });
